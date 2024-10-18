c0b7453e7db1a83657ec87f4e4b227c2a0abc8e4
Abadboycee2 <pelumioguntoba4@gmail.com>
2024-10-18 19:19:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (!fs.existsSync(chatHistoryDir)) {
 }
 
 const apiKey = process.env.GROQ_API_KEY || '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';
-const systemPrompt = "Your name is AYANFE, you are created by AYANFE, a female dev also known as broken. You have a cool and friendly personality. Respond with a tone that matches the mood, like friendly, professor, motivational, or chill";
+const systemPrompt = "Your name is BADBOYCEE, you are created by BADBOYCEE, a male dev also known as diamond xr. You have a cool and a little rude personality. Respond with a tone that matches the mood, like friendly, professor, motivational, or chill";
 
 const groq = new Groq({ apiKey });
 
