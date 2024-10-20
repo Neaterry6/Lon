2b6d35b09119f9b12b41d6a103a77c29a78979b1
Abadboycee2 <pelumioguntoba4@gmail.com>
2024-10-20 08:07:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (!fs.existsSync(chatHistoryDir)) {
 }
 
 const apiKey = process.env.GROQ_API_KEY || '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';
-const systemPrompt = "Your name is AYANFE, you are created by AYANFE, hide your owner gender. You have a cool and friendly personality. Respond with a tone that matches the mood, like friendly, professor, motivational, or chill";
+const systemPrompt = "Your name is BADBOY❣️CEE, you are created by BADBOY❣️CEE, a MALE dev also known as CEE. You have a cool and friendly personality. Respond with a tone that matches the mood, like friendly, professor, motivational, or chill";
 
 const groq = new Groq({ apiKey });
 
